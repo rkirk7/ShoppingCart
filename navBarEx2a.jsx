160dc4669d796c48f91f52c343e489ae22330440
rkirk7 <129214148+rkirk7@users.noreply.github.com>
2024-04-12 13:09:06 -0400

    Initial files for the shopping cart exercise

---
+++ navBarEx2a.jsx
@@ -0,0 +1,49 @@
+// Ex 2 - remove any item from navbar with less than 2 in stock
+function NavBar({ stockitems, minstock }) {
+  const [stock, setStock] = React.useState(stockitems);
+  const [cart, setCart] = React.useState([]);
+  const { Button } = ReactBootstrap;
+
+  const cartItem = e => {
+    let [name, num] = e.target.innerHTML.split(":");
+    let theItem = stock.filter(item => item.name == name);
+    setCart([...cart, ...theItem]);
+  };
+
+  let filteredList = stock.filter(element => element.instock >= minstock);
+
+  let list2 = cart.map((item, index) => {
+    return (
+      <Button onClick={cartItem} key={index.toString()}>
+        {item.name}:{item.instock}
+      </Button>
+    );
+  });
+
+  let list1 = filteredList.map((item, index) => {
+    return (
+      <Button onClick={cartItem} key={index.toString()}>
+        {item.name}:{item.instock}
+      </Button>
+    );
+  });
+  // note that React needs to have a single Parent
+  return (
+    <>
+      <ul style={{ listStyleType: "none" }}>{list1}</ul>
+      <h1>You Picked</h1>
+      <div>{list2}</div>
+    </>
+  );
+}
+const menuItems = [
+  { name: "apple", instock: 2 },
+  { name: "pineapple", instock: 3 },
+  { name: "pear", instock: 0 },
+  { name: "peach", instock: 3 },
+  { name: "orange", instock: 1 }
+];
+ReactDOM.render(
+  <NavBar stockitems={menuItems} minstock={0} />,
+  document.getElementById("root")
+);
---
+++ navBarNoevensEx01.jsx
@@ -0,0 +1,18 @@
+// Ex 1 - remove all even numbers from the NavBar
+// then make a Button for each instead of an <li>
+function NavBar({ menuitems }) {
+  const { Button } = ReactBootstrap;
+
+  const filteredList = menuitems.filter(item => item %2 !== 0)
+
+  const updatedList = filteredList.map((listItems, index) => {
+    return <button key={index.toString()}>{listItems}</button>;
+  });
+  // note that React needs to have a single Parent
+  return <ul style={{ listStyleType: "none" }}>{updatedList}</ul>;
+}
+const menuItems = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NavBar menuitems={menuItems} />,
+  document.getElementById("root")
+);
